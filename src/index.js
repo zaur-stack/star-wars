dd61b2e19557e5ac6439784a8cc2635c87cf9993
zaur bersanov <storeborz@mail.ru>
2024-01-05 04:49:44 +0300

    add deploy scripts

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    <BrowserRouter>
+    <BrowserRouter basename='/star-wars/'>
       <Provider store={store}>
         <ThemeProvider>
           <App />
