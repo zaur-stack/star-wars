63323c6b7bd6252cb551bce6e110d49be2c0da26
zaur bersanov <storeborz@mail.ru>
2024-01-05 17:59:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '@containers/App/App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store'
 import { ThemeProvider } from './context/ThemeProvider';
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    <HashRouter basename='/star-wars/'>
+    <HashRouter>
       <Provider store={store}>
         <ThemeProvider>
           <App />
