8c197ea38691467c479d2f7c636046fe2f1dfa10
zaur bersanov <storeborz@mail.ru>
2024-01-05 16:48:54 +0300

    Updates

--- src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-// import favouriteReducer from './favouriteReducer';
-// import { combineReducers } from 'redux';
-
-// export default combineReducers({
-//   favouriteReducer: favouriteReducer
-// })
